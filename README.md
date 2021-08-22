ab0bfce9c5f8895541b3dc4f14e3dcf0efa38505
Frank Heller <f.heller@brighter.cloud>
2021-08-22 19:16:39 +0200

    changed auth setting

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Node.js oAuth Demo App - Salesforce as IDP
 
-This is a demo application that showcases an implementation of oAuth from Salesforce in Node.js. 
+This is a demo application that showcases an implementation of oAuth from Salesforce in Node.js.
 
 ## Installation
+
 Firstly, setup your Salesforce Org with a [connected app](https://trailhead.salesforce.com/en/projects/workshop-electric-imp/steps/connected-app-setup). Remember! You will have to come back here and update your callback URL once you get it.
 
 ### Building the App locally
@@ -11,42 +12,51 @@ Before you start, make sure you've got Redis running locally. (If not, it's okay
 
 Firstly, clone (or fork and clone) the App.
 
-````
+```
 
 git clone https://github.com/adamSellers/Salesforce-oAuth-Demo.git
-````
+```
 
 Change directories and install stuff
-````
+
+```
 cd Salesforce-oAuth-Demo && npm install
-````
+```
+
 Create the .env file
-````
+
+```
 touch .env
-````
+```
+
 Then add the following config vars
-````
+
+```
 CLIENTID={your salesforce client id}
 CLIENTSECRET={your salesforce client secret}
 CALLBACKURL=http://localhost:3001/salesforce/auth
 REDISSECRET={A super secret string}
 REDIS_URL={your redis URL}
 SALESFORCE_ENDPOINT={your salesforce login endpoint, ie https://login.salesforce.com or a community URL}
-````
+```
+
 remember, head back to your connected app and update that Callback URL!
 
 Once all that is done, you are good to go!
-````
+
+```
 npm start
-````
+```
+
 Then navigate to [http://localhost:3001](http://localhost:3001) to get started.
 
-### Or you can do it much easier in Heroku.. 
-[![Heroku Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/adamSellers/Salesforce-oAuth-Demo.git) 
+### Or you can do it much easier in Heroku..
+
+[![Heroku Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/BrighterCloud/Salesforce-oAuth-Demo.git)
 
 Once you have your app, be sure to set the following Config Vars:
 
-1. CLIENTDID: This is your Salesforce connected app client ID. 
+1. CLIENTDID: This is your Salesforce connected app client ID.
 2. CLIENTSECRET: This is your Salesforce connected app secret.
 3. CALLBACKURL: this will be https://{YOURAPPNAME}.herokuapp.com/salesforce/auth
 4. SALESFORCE_ENDPOINT: this is your login endpoint for SF (eg: https://login.salesforce.com or a community URL)
--- routes/auth.js
+++ routes/auth.js
@@ -24,7 +24,7 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-var request = require('request');
+var request = require("request");
 
 var clientId = process.env.CLIENTID;
 var clientSecret = process.env.CLIENTSECRET;
@@ -32,91 +32,104 @@ var callbackUrl = process.env.CALLBACKURL;
 
 var salesforceEndpoint = process.env.SALESFORCE_ENDPOINT;
 
-function login (req, res, next) {
-    // we simply just redirect the user to the Salesforce login service.
-    res.redirect(`${salesforceEndpoint}/services/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${callbackUrl}&display=popup`);
-};
+function login(req, res, next) {
+  // we simply just redirect the user to the Salesforce login service.
+  res.redirect(
+    `${salesforceEndpoint}/services/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${callbackUrl}&display=popup`
+  );
+}
 
-function callback (req, res, next) {
-    // first we harvest the code variable
-    let authCode = req.query.code;
+function callback(req, res, next) {
+  // first we harvest the code variable
+  let authCode = req.query.code;
 
-    // the URL that we are going to post the auth code to
-    let tokenUrl = `${salesforceEndpoint}/services/oauth2/token`;
-    // post to token url to get access token using the request library
-    request.post(
-        tokenUrl,
-        {
-            form: {
-                code: authCode,
-                grant_type: 'authorization_code',
-                client_id: clientId,
-                client_secret: clientSecret,
-                redirect_uri: callbackUrl
-            },
-            json: true
-        },
-        function (err, data) {
-            // handle errors if present
-            if (err) {
-                return next(err);
-            }
-            // set session data in redis
-            req.session.loginCounter++;
-            req.session.authInfo = {
-                accessToken: data.body.access_token,
-                refreshToken: data.body.refresh_token,
-                userId: data.body.id,
-                signature: data.body.signature,
-                instanceUrl: data.body.instance_url,
-                issuedAt: data.body.issued_at,
-                userIsAuthenticated: true
-            }
-            req.session.save();
-            console.log('session data saved as: ' + JSON.stringify(req.session.authInfo));
-            // send user back to homepage to view stats
-            res.redirect('/');
-        });
-};
+  // the URL that we are going to post the auth code to
+  let tokenUrl = `${salesforceEndpoint}/services/oauth2/token`;
+  // post to token url to get access token using the request library
+
+  console.log(authCode);
+  console.log(tokenUrl);
+  request.post(
+    tokenUrl,
+    {
+      form: {
+        code: authCode,
+        grant_type: "authorization_code",
+        client_id: clientId,
+        client_secret: clientSecret,
+        redirect_uri: callbackUrl,
+      },
+      json: true,
+    },
+    function (err, data) {
+      // handle errors if present
+      if (err) {
+        return next(err);
+      }
+      // set session data in redis
+      console.log("session data: " + JSON.stringify(data));
+      req.session.loginCounter++;
+      req.session.authInfo = {
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        userId: data.body.id,
+        signature: data.body.signature,
+        instanceUrl: data.body.instance_url,
+        issuedAt: data.body.issued_at,
+        userIsAuthenticated: true,
+      };
+      req.session.save();
+      console.log(
+        "session data saved as: " + JSON.stringify(req.session.authInfo)
+      );
+      // send user back to homepage to view stats
+      res.redirect("/");
+    }
+  );
+}
 
 // logout function that revokes the oAuth token from Salesforce
-function logout (req, res, next) {
-    // first define the URL for the token revoke service
-    let revokeUrl = `${salesforceEndpoint}/services/oauth2/revoke`;
+function logout(req, res, next) {
+  // first define the URL for the token revoke service
+  let revokeUrl = `${salesforceEndpoint}/services/oauth2/revoke`;
 
-    // POST request with data
-    request.post(revokeUrl, {
-        form: {
-            token: req.session.authInfo.accessToken
-        }
-    }, function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('everything is invalidated: ' + JSON.stringify(data));
-        // destroy the session and redirect to root
-        req.session.destroy();
-        res.redirect('/');
-    });
+  // POST request with data
+  request.post(
+    revokeUrl,
+    {
+      form: {
+        token: req.session.authInfo.accessToken,
+      },
+    },
+    function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("everything is invalidated: " + JSON.stringify(data));
+      // destroy the session and redirect to root
+      req.session.destroy();
+      res.redirect("/");
+    }
+  );
 }
 
 // function to check if logged in
-function isLoggedIn (err, req, res, next) {
-    if (err) {
-        return next(err);
-    }
-    if (!req.session.authInfo.userIsAuthenticated) {
-        console.log('user must login first!');
-        res.redirect('/');
-    } else {
-        next();
-    }
+function isLoggedIn(err, req, res, next) {
+  if (err) {
+    return next(err);
+  }
+  if (!req.session.authInfo.userIsAuthenticated) {
+    console.log("user must login first!");
+    res.redirect("/");
+  } else {
+    next();
+  }
 }
 
 // Export the functions to be used in the router.
 module.exports = {
-    login: login,
-    callback: callback,
-    logout: logout,
-    isLoggedIn: isLoggedIn
-}
+  login: login,
+  callback: callback,
+  logout: logout,
+  isLoggedIn: isLoggedIn,
+};
